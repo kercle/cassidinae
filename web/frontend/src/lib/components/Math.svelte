ba704ef4ba64ac0d3f32572d63568a6dfd287b27
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-20 19:44:20 +0100

    Make ResultCell font in web frontend smaller.

--- web/frontend/src/lib/components/Math.svelte
+++ web/frontend/src/lib/components/Math.svelte
@@ -3,9 +3,9 @@
 	export let expr = '';
 </script>
 
-<div class="math-container flex w-full">
+<div class="math-container flex w-full" style="font-size: 0.9rem;">
 	{@html katex.renderToString(expr, {
 		throwOnError: false,
-		displayMode: true // This is the magic switch
+		displayMode: true
 	})}
 </div>
