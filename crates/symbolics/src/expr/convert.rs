55fb0b2c67296698b4b813265dbeed6f0f71863f
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-02-22 00:47:18 +0100

    Move missing converters for Expr in module.

--- crates/symbolics/src/expr/convert.rs
+++ crates/symbolics/src/expr/convert.rs
@@ -1,4 +1,10 @@
-use crate::{atom::Atom, expr::Expr};
+use numbers::Number;
+
+use crate::{
+    atom::Atom,
+    expr::Expr,
+    parser::ast::{ADD_HEAD, MUL_HEAD, POW_HEAD, ParserAst},
+};
 
 impl<A, T: Into<Atom>> From<T> for Expr<A>
 where
@@ -8,6 +14,117 @@ where
         Expr::Atom {
             entry: x.into(),
             annotation: A::default(),
+        }
+    }
+}
+
+impl<A> Expr<A>
+where
+    A: Default + Clone + PartialEq,
+{
+    pub fn from_i64(value: i64) -> Self {
+        Self::new_number(Number::from_i64(value))
+    }
+
+    pub fn from_parser_ast(parser_ast: &ParserAst<A>) -> Self {
+        match parser_ast {
+            ParserAst::Constant { value, annotation } => {
+                Self::new_number(value.clone()).with_annotation(annotation.clone())
+            }
+            ParserAst::Symbol { name, annotation } => {
+                Self::new_symbol(name.clone()).with_annotation(annotation.clone())
+            }
+            ParserAst::Add { nodes, annotation } => {
+                let head = Self::new_symbol(ADD_HEAD);
+                let args = nodes
+                    .iter()
+                    .map(|node| Self::from_parser_ast(node))
+                    .collect();
+                Self::new_compound(head, args).with_annotation(annotation.clone())
+            }
+            ParserAst::Sub {
+                lhs,
+                rhs,
+                annotation,
+            } => {
+                let head = Self::new_symbol(ADD_HEAD);
+                let lhs = Self::from_parser_ast(lhs.as_ref());
+                let rhs = Self::from_parser_ast(rhs.as_ref());
+
+                Self::new_compound(
+                    head,
+                    vec![
+                        lhs,
+                        Self::new_compound(
+                            Self::new_symbol(MUL_HEAD),
+                            vec![Self::new_number(Number::from_i64(-1)), rhs],
+                        ),
+                    ],
+                )
+                .with_annotation(annotation.clone())
+            }
+            ParserAst::Negation { arg, annotation } => {
+                let arg = Self::from_parser_ast(arg.as_ref());
+                Self::new_compound(
+                    Self::new_symbol(MUL_HEAD),
+                    vec![Self::new_number(Number::from_i64(-1)), arg],
+                )
+                .with_annotation(annotation.clone())
+            }
+            ParserAst::Mul { nodes, annotation } => {
+                let head = Self::new_symbol(MUL_HEAD);
+                let args = nodes
+                    .iter()
+                    .map(|node| Self::from_parser_ast(node))
+                    .collect();
+                Self::new_compound(head, args).with_annotation(annotation.clone())
+            }
+            ParserAst::Div {
+                lhs,
+                rhs,
+                annotation,
+            } => {
+                let head = Self::new_symbol(MUL_HEAD);
+                let lhs = Self::from_parser_ast(lhs.as_ref());
+                let rhs = Self::from_parser_ast(rhs.as_ref());
+
+                Self::new_compound(
+                    head,
+                    vec![
+                        lhs,
+                        Self::new_compound(
+                            Self::new_symbol(POW_HEAD),
+                            vec![rhs, Self::new_number(Number::from_i64(-1))],
+                        ),
+                    ],
+                )
+                .with_annotation(annotation.clone())
+            }
+            ParserAst::Pow {
+                lhs,
+                rhs,
+                annotation,
+            } => {
+                let head = Self::new_symbol(POW_HEAD);
+                let lhs = Self::from_parser_ast(lhs.as_ref());
+                let rhs = Self::from_parser_ast(rhs.as_ref());
+
+                Self::new_compound(head, vec![lhs, rhs]).with_annotation(annotation.clone())
+            }
+            ParserAst::FunctionCall {
+                name,
+                args,
+                annotation,
+            } => {
+                let head = Self::new_symbol(name);
+                let args = args
+                    .iter()
+                    .map(|node| Self::from_parser_ast(node))
+                    .collect();
+
+                Self::new_compound(head, args).with_annotation(annotation.clone())
+            }
+            ParserAst::Block { .. } => todo!(),
         }
     }
 }
--- crates/symbolics/src/expr/mod.rs
+++ crates/symbolics/src/expr/mod.rs
@@ -9,11 +9,7 @@ pub mod ops;
 pub mod types;
 pub mod walk;
 
-use numbers::Number;
-
 pub use types::*;
-
-use crate::parser::ast::{ADD_HEAD, MUL_HEAD, POW_HEAD, ParserAst};
 
 impl<A> Expr<A>
 where
@@ -26,112 +22,6 @@ where
         } else {
             false
         }
-    }
-
-    pub fn from_parser_ast(parser_ast: &ParserAst<A>) -> Self {
-        match parser_ast {
-            ParserAst::Constant { value, annotation } => {
-                Self::new_number(value.clone()).with_annotation(annotation.clone())
-            }
-            ParserAst::Symbol { name, annotation } => {
-                Self::new_symbol(name.clone()).with_annotation(annotation.clone())
-            }
-            ParserAst::Add { nodes, annotation } => {
-                let head = Self::new_symbol(ADD_HEAD);
-                let args = nodes
-                    .iter()
-                    .map(|node| Self::from_parser_ast(node))
-                    .collect();
-                Self::new_compound(head, args).with_annotation(annotation.clone())
-            }
-            ParserAst::Sub {
-                lhs,
-                rhs,
-                annotation,
-            } => {
-                let head = Self::new_symbol(ADD_HEAD);
-                let lhs = Self::from_parser_ast(lhs.as_ref());
-                let rhs = Self::from_parser_ast(rhs.as_ref());
-
-                Self::new_compound(
-                    head,
-                    vec![
-                        lhs,
-                        Self::new_compound(
-                            Self::new_symbol(MUL_HEAD),
-                            vec![Self::new_number(Number::from_i64(-1)), rhs],
-                        ),
-                    ],
-                )
-                .with_annotation(annotation.clone())
-            }
-            ParserAst::Negation { arg, annotation } => {
-                let arg = Self::from_parser_ast(arg.as_ref());
-                Self::new_compound(
-                    Self::new_symbol(MUL_HEAD),
-                    vec![Self::new_number(Number::from_i64(-1)), arg],
-                )
-                .with_annotation(annotation.clone())
-            }
-            ParserAst::Mul { nodes, annotation } => {
-                let head = Self::new_symbol(MUL_HEAD);
-                let args = nodes
-                    .iter()
-                    .map(|node| Self::from_parser_ast(node))
-                    .collect();
-                Self::new_compound(head, args).with_annotation(annotation.clone())
-            }
-            ParserAst::Div {
-                lhs,
-                rhs,
-                annotation,
-            } => {
-                let head = Self::new_symbol(MUL_HEAD);
-                let lhs = Self::from_parser_ast(lhs.as_ref());
-                let rhs = Self::from_parser_ast(rhs.as_ref());
-
-                Self::new_compound(
-                    head,
-                    vec![
-                        lhs,
-                        Self::new_compound(
-                            Self::new_symbol(POW_HEAD),
-                            vec![rhs, Self::new_number(Number::from_i64(-1))],
-                        ),
-                    ],
-                )
-                .with_annotation(annotation.clone())
-            }
-            ParserAst::Pow {
-                lhs,
-                rhs,
-                annotation,
-            } => {
-                let head = Self::new_symbol(POW_HEAD);
-                let lhs = Self::from_parser_ast(lhs.as_ref());
-                let rhs = Self::from_parser_ast(rhs.as_ref());
-
-                Self::new_compound(head, vec![lhs, rhs]).with_annotation(annotation.clone())
-            }
-            ParserAst::FunctionCall {
-                name,
-                args,
-                annotation,
-            } => {
-                let head = Self::new_symbol(name);
-                let args = args
-                    .iter()
-                    .map(|node| Self::from_parser_ast(node))
-                    .collect();
-
-                Self::new_compound(head, args).with_annotation(annotation.clone())
-            }
-            ParserAst::Block { .. } => todo!(),
-        }
-    }
-
-    pub fn from_i64(value: i64) -> Self {
-        Self::new_number(Number::from_i64(value))
     }
 
     pub fn annotation_to_default(self) -> Self {
