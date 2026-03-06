ac36ad06d5f943f7a0ded5e57d8b2c050c432b3f
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-03-06 20:26:20 +0100

    Transfer parser code to new sub-library.

---
+++ crates/parser/src/ast.rs
@@ -0,0 +1,414 @@
+use std::fmt::Debug;
+
+use numbers::Number;
+
+pub const ADD_HEAD: &str = "Add";
+pub const SUB_HEAD: &str = "Sub";
+pub const MUL_HEAD: &str = "Mul";
+pub const DIV_HEAD: &str = "Div";
+pub const NEG_HEAD: &str = "Neg";
+pub const POW_HEAD: &str = "Pow";
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParserAst<Annotation = ()>
+where
+    Annotation: Clone + PartialEq,
+{
+    Constant {
+        value: Number,
+        annotation: Annotation,
+    },
+    Symbol {
+        name: String,
+        annotation: Annotation,
+    },
+    Add {
+        nodes: Vec<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Negation {
+        arg: Box<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Sub {
+        lhs: Box<ParserAst<Annotation>>,
+        rhs: Box<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Mul {
+        nodes: Vec<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Div {
+        lhs: Box<ParserAst<Annotation>>,
+        rhs: Box<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Pow {
+        lhs: Box<ParserAst<Annotation>>,
+        rhs: Box<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    FunctionCall {
+        name: String,
+        args: Vec<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+    Block {
+        nodes: Vec<ParserAst<Annotation>>,
+        annotation: Annotation,
+    },
+}
+
+impl<A> ParserAst<A>
+where
+    A: Default + Clone + PartialEq,
+{
+    pub fn new_constant(value: Number) -> Self {
+        ParserAst::Constant {
+            annotation: A::default(),
+            value,
+        }
+    }
+
+    pub fn new_constant_from_i64(value: i64) -> Self {
+        ParserAst::new_constant(Number::from_i64(value))
+    }
+
+    pub fn new_constant_one() -> Self {
+        Self::new_constant_from_i64(1)
+    }
+
+    pub fn new_constant_zero() -> Self {
+        Self::new_constant_from_i64(0)
+    }
+
+    pub fn new_named_value<T: ToString>(name: T) -> Self {
+        ParserAst::Symbol {
+            name: name.to_string(),
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_add(nodes: Vec<ParserAst<A>>) -> Self {
+        ParserAst::Add {
+            nodes,
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_add_pair(lhs: ParserAst<A>, rhs: ParserAst<A>) -> Self {
+        Self::new_add(vec![lhs, rhs])
+    }
+
+    pub fn new_negation(arg: ParserAst<A>) -> Self {
+        ParserAst::Negation {
+            arg: Box::new(arg),
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_sub(lhs: ParserAst<A>, rhs: ParserAst<A>) -> Self {
+        ParserAst::Sub {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_mul_pair(lhs: ParserAst<A>, rhs: ParserAst<A>) -> Self {
+        Self::new_mul(vec![lhs, rhs])
+    }
+
+    pub fn new_mul(nodes: Vec<ParserAst<A>>) -> Self {
+        ParserAst::Mul {
+            nodes,
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_div(lhs: ParserAst<A>, rhs: ParserAst<A>) -> Self {
+        ParserAst::Div {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_pow(lhs: ParserAst<A>, rhs: ParserAst<A>) -> Self {
+        ParserAst::Pow {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_cos(arg: ParserAst<A>) -> Self {
+        Self::new_function_call("cos".to_string(), vec![arg])
+    }
+
+    pub fn new_sin(arg: ParserAst<A>) -> Self {
+        Self::new_function_call("sin".to_string(), vec![arg])
+    }
+
+    pub fn new_tan(arg: ParserAst<A>) -> Self {
+        Self::new_function_call("tan".to_string(), vec![arg])
+    }
+
+    pub fn new_sqrt(arg: ParserAst<A>) -> Self {
+        Self::new_function_call("sqrt".to_string(), vec![arg])
+    }
+
+    pub fn new_function_call<T: ToString>(name: T, args: Vec<ParserAst<A>>) -> Self {
+        ParserAst::FunctionCall {
+            name: name.to_string(),
+            args,
+            annotation: A::default(),
+        }
+    }
+
+    pub fn new_block(nodes: Vec<ParserAst<A>>) -> Self {
+        ParserAst::Block {
+            nodes,
+            annotation: A::default(),
+        }
+    }
+
+    pub fn drop_annotation(self) -> ParserAst {
+        self.map_annotation(&mut |_| ())
+    }
+
+    pub fn with_annotation(self, annotation: A) -> Self {
+        use ParserAst::*;
+        match self {
+            Constant { value, .. } => Constant { value, annotation },
+            Symbol { name, .. } => Symbol { name, annotation },
+            Add { nodes, .. } => Add { nodes, annotation },
+            Negation { arg, .. } => Negation { arg, annotation },
+            Sub { lhs, rhs, .. } => Sub {
+                lhs,
+                rhs,
+                annotation,
+            },
+            Mul { nodes, .. } => Mul { nodes, annotation },
+            Div { lhs, rhs, .. } => Div {
+                lhs,
+                rhs,
+                annotation,
+            },
+            Pow { lhs, rhs, .. } => Pow {
+                lhs,
+                rhs,
+                annotation,
+            },
+            FunctionCall { name, args, .. } => FunctionCall {
+                name,
+                args,
+                annotation,
+            },
+            Block { nodes, annotation } => Block { nodes, annotation },
+        }
+    }
+
+    pub fn annotation(&self) -> &A {
+        use ParserAst::*;
+        match self {
+            Constant { annotation, .. }
+            | Symbol { annotation, .. }
+            | Add { annotation, .. }
+            | Negation { annotation, .. }
+            | Sub { annotation, .. }
+            | Mul { annotation, .. }
+            | Div { annotation, .. }
+            | Pow { annotation, .. }
+            | FunctionCall { annotation, .. }
+            | Block { annotation, .. } => annotation,
+        }
+    }
+
+    pub fn value_from_constant(&self) -> Option<Number> {
+        if let ParserAst::Constant { value, .. } = self {
+            Some(value.clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn map<F>(self, mut f: F) -> Self
+    where
+        F: FnMut(Self) -> Self,
+    {
+        self.map_inner(&mut f)
+    }
+
+    fn map_inner<F>(self, f: &mut F) -> Self
+    where
+        F: FnMut(Self) -> Self,
+    {
+        use ParserAst::*;
+        let mapped = match self {
+            Add { nodes, annotation } => ParserAst::Add {
+                nodes: nodes.into_iter().map(|n| n.map_inner(f)).collect(),
+                annotation,
+            },
+            Negation { arg, annotation } => Negation {
+                arg: Box::new(arg.map_inner(f)),
+                annotation,
+            },
+            Sub {
+                lhs,
+                rhs,
+                annotation,
+            } => Sub {
+                lhs: Box::new(lhs.map_inner(f)),
+                rhs: Box::new(rhs.map_inner(f)),
+                annotation,
+            },
+            Mul { nodes, annotation } => Mul {
+                nodes: nodes.into_iter().map(|n| n.map_inner(f)).collect(),
+                annotation,
+            },
+            Div {
+                lhs,
+                rhs,
+                annotation,
+            } => Div {
+                lhs: Box::new(lhs.map_inner(f)),
+                rhs: Box::new(rhs.map_inner(f)),
+                annotation,
+            },
+            Pow {
+                lhs,
+                rhs,
+                annotation,
+            } => Pow {
+                lhs: Box::new(lhs.map_inner(f)),
+                rhs: Box::new(rhs.map_inner(f)),
+                annotation,
+            },
+            FunctionCall {
+                name,
+                args,
+                annotation,
+            } => FunctionCall {
+                name,
+                args: args.into_iter().map(|a| a.map_inner(f)).collect(),
+                annotation,
+            },
+            Block { nodes, annotation } => Block {
+                nodes: nodes.into_iter().map(|n| n.map_inner(f)).collect(),
+                annotation,
+            },
+            node @ Constant { .. } | node @ Symbol { .. } => node,
+        };
+
+        f(mapped)
+    }
+}
+
+impl<A> ParserAst<A>
+where
+    A: Clone + PartialEq,
+{
+    pub fn is_constant(&self) -> bool {
+        matches!(self, ParserAst::Constant { .. })
+    }
+
+    pub fn is_one(&self) -> bool {
+        if let ParserAst::Constant { value, .. } = self {
+            value.is_one()
+        } else {
+            false
+        }
+    }
+
+    pub fn is_zero(&self) -> bool {
+        if let ParserAst::Constant { value, .. } = self {
+            value.is_zero()
+        } else {
+            false
+        }
+    }
+
+    pub fn is_symbol(&self) -> bool {
+        matches!(self, ParserAst::Symbol { .. })
+    }
+
+    pub fn matches_symbol<T: AsRef<str>>(&self, x: T) -> bool {
+        if let ParserAst::Symbol { name, .. } = self {
+            name == x.as_ref()
+        } else {
+            false
+        }
+    }
+
+    pub fn map_annotation<B, F>(self, f: &mut F) -> ParserAst<B>
+    where
+        B: Clone + PartialEq,
+        F: FnMut(A) -> B,
+    {
+        use ParserAst::*;
+        match self {
+            Constant { value, annotation } => Constant {
+                value,
+                annotation: f(annotation),
+            },
+            Symbol { name, annotation } => Symbol {
+                name,
+                annotation: f(annotation),
+            },
+            Add { nodes, annotation } => Add {
+                nodes: nodes.into_iter().map(|n| n.map_annotation(f)).collect(),
+                annotation: f(annotation),
+            },
+            Negation { arg, annotation } => Negation {
+                arg: Box::new(arg.map_annotation(f)),
+                annotation: f(annotation),
+            },
+            Sub {
+                lhs,
+                rhs,
+                annotation,
+            } => Sub {
+                lhs: Box::new(lhs.map_annotation(f)),
+                rhs: Box::new(rhs.map_annotation(f)),
+                annotation: f(annotation),
+            },
+            Mul { nodes, annotation } => Mul {
+                nodes: nodes.into_iter().map(|n| n.map_annotation(f)).collect(),
+                annotation: f(annotation),
+            },
+            Div {
+                lhs,
+                rhs,
+                annotation,
+            } => Div {
+                lhs: Box::new(lhs.map_annotation(f)),
+                rhs: Box::new(rhs.map_annotation(f)),
+                annotation: f(annotation),
+            },
+            Pow {
+                lhs,
+                rhs,
+                annotation,
+            } => Pow {
+                lhs: Box::new(lhs.map_annotation(f)),
+                rhs: Box::new(rhs.map_annotation(f)),
+                annotation: f(annotation),
+            },
+            FunctionCall {
+                name,
+                args,
+                annotation,
+            } => FunctionCall {
+                name,
+                args: args.into_iter().map(|a| a.map_annotation(f)).collect(),
+                annotation: f(annotation),
+            },
+            Block { nodes, annotation } => Block {
+                nodes: nodes.into_iter().map(|n| n.map_annotation(f)).collect(),
+                annotation: f(annotation),
+            },
+        }
+    }
+}
---
+++ crates/parser/src/lib.rs
@@ -0,0 +1,427 @@
+pub mod ast;
+pub mod ast_ops;
+pub mod error;
+pub mod fmt;
+pub mod lex;
+
+use std::str::FromStr;
+
+use numbers::Number;
+
+use crate::{
+    ast::ParserAst,
+    error::{BoxedError, ParseError},
+    lex::{Token, TokenStream},
+};
+
+fn parse_identifier_or_call(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <named_value_or_function_call> ::= <identifier>
+    //    | <identifier> "[" "]"
+    //    | <identifier> "[" <block> { "," <block> }* ")"
+
+    let identifier = stream.next_if_identifier();
+
+    let Some(identifier) = identifier.map(|i| i.to_string()) else {
+        return Err(ParseError {
+            message: "Expected an identifier".to_string(),
+            at_token: stream.peek().cloned(),
+        });
+    };
+
+    if stream.next_if_matches_token(&Token::LeftBracket).is_none() {
+        return Ok(ParserAst::new_named_value(identifier));
+    }
+
+    if stream.next_if_matches_token(&Token::RightBracket).is_some() {
+        return Ok(ParserAst::new_function_call(identifier, vec![]));
+    }
+
+    let mut args = vec![parse_block(stream)?];
+    loop {
+        if stream.next_if_matches_token(&Token::Comma).is_none() {
+            break; // No more arguments
+        }
+
+        args.push(parse_block(stream)?);
+    }
+
+    if stream.next_if_matches_token(&Token::RightBracket).is_none() {
+        return Err(ParseError {
+            message: "Expected closing parenthesis ']' after function arguments".to_string(),
+            at_token: stream.peek().cloned(),
+        });
+    }
+
+    Ok(ParserAst::new_function_call(identifier, args))
+}
+
+fn parse_atom(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <atom> ::= <number>
+    //    | "(" <block> ")"
+    //    | <named_value_or_function_call>
+
+    if stream.next_if_matches_token(&Token::LeftParen).is_some() {
+        let expr = parse_block(stream)?;
+
+        if stream.next_if_matches_token(&Token::RightParen).is_some() {
+            Ok(expr)
+        } else {
+            Err(ParseError {
+                message: "Expected closing parenthesis ')'".to_string(),
+                at_token: stream.peek().cloned(),
+            })
+        }
+    } else if let Some(value) = stream.next_if_number() {
+        let value = Number::from_str(value).map_err(|e| ParseError {
+            message: e,
+            at_token: stream.peek().cloned(),
+        })?;
+        Ok(ParserAst::new_constant(value))
+    } else {
+        parse_identifier_or_call(stream)
+    }
+}
+
+fn parse_power(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <power> ::= <atom> { "^" <power> }
+
+    let mut result = parse_atom(stream)?;
+
+    if stream.next_if_matches_token(&Token::Caret).is_some() {
+        result = ParserAst::new_pow(result, parse_power(stream)?);
+    }
+
+    Ok(result)
+}
+
+fn parse_signed_power(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <signed_power> ::= { "+" | "-" }* power
+
+    let mut negate_count = 0;
+
+    while let Some(sign) =
+        stream.next_if_matches(|token| matches!(token, Token::Plus | Token::Minus))
+    {
+        if *sign == Token::Minus {
+            negate_count += 1;
+        }
+    }
+
+    let ast = parse_power(stream)?;
+
+    if negate_count % 2 == 1 {
+        Ok(ParserAst::new_negation(ast))
+    } else {
+        Ok(ast)
+    }
+}
+
+fn parse_product(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <product> ::= <match_signed_power> { ("*"|"/") <match_signed_power> }*
+
+    let mut result = parse_signed_power(stream)?;
+
+    loop {
+        let c = stream.next_if_matches(|token| matches!(token, Token::Asterisk | Token::Slash));
+
+        result = match c {
+            Some(Token::Asterisk) => ParserAst::new_mul_pair(result, parse_signed_power(stream)?),
+            Some(Token::Slash) => ParserAst::new_div(result, parse_signed_power(stream)?),
+            None => break,
+            _ => unreachable!(),
+        };
+    }
+
+    Ok(result)
+}
+
+fn parse_sum(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <sum> ::= <product> { ("+"|"-") <product> }*
+
+    let mut result = parse_product(stream)?;
+
+    loop {
+        let op = stream.next_if_matches(|token| matches!(token, Token::Plus | Token::Minus));
+
+        result = match op {
+            Some(Token::Plus) => ParserAst::new_add_pair(result, parse_product(stream)?),
+            Some(Token::Minus) => ParserAst::new_sub(result, parse_product(stream)?),
+            None => break, // No more operators
+            _ => unreachable!(),
+        };
+    }
+
+    Ok(result)
+}
+
+fn parse_expression(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <expression> ::= <sum>
+
+    parse_sum(stream)
+}
+
+fn parse_block(stream: &mut TokenStream) -> Result<ParserAst, ParseError> {
+    // <block> ::= <expression> { ";" <expression> }*
+    //    | "{" <block> "}"
+
+    let mut nodes = Vec::new();
+
+    if stream.next_if_matches_token(&Token::LeftBrace).is_some() {
+        let block = parse_block(stream)?;
+        if stream.next_if_matches_token(&Token::RightBrace).is_none() {
+            return Err(ParseError {
+                message: "Expected closing brace '}'".to_string(),
+                at_token: stream.peek().cloned(),
+            });
+        }
+
+        Ok(block)
+    } else {
+        loop {
+            let expr = parse_expression(stream)?;
+            nodes.push(expr);
+
+            if stream.next_if_matches_token(&Token::Semicolon).is_none() {
+                break; // No more expressions
+            }
+        }
+
+        if nodes.is_empty() {
+            Err(ParseError {
+                message: "Expected at least one expression in the block".to_string(),
+                at_token: stream.peek().cloned(),
+            })
+        } else if nodes.len() == 1 {
+            Ok(nodes.pop().unwrap())
+        } else {
+            Ok(ParserAst::new_block(nodes))
+        }
+    }
+}
+
+pub fn parse(input: &str) -> Result<ParserAst, BoxedError> {
+    let mut stream = TokenStream::from_str(input)?;
+
+    let ast = parse_block(&mut stream);
+
+    if ast.is_ok() && !stream.end_of_stream() {
+        return Err(ParseError {
+            message: "Unexpected tokens at the end of input".to_string(),
+            at_token: stream.peek().cloned(),
+        }
+        .into());
+    }
+
+    match ast {
+        Ok(node) => Ok(node),
+        Err(err) => Err(err.into()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_expression_long() {
+        let input = "3 + 4 * 2 - (1 + 5) ^ 2 ^ 3";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_sub(
+                ParserAst::new_add_pair(
+                    ParserAst::new_constant(Number::from_str("3").unwrap()),
+                    ParserAst::new_mul_pair(
+                        ParserAst::new_constant(Number::from_str("4").unwrap()),
+                        ParserAst::new_constant(Number::from_str("2").unwrap()),
+                    )
+                ),
+                ParserAst::new_pow(
+                    ParserAst::new_add_pair(
+                        ParserAst::new_constant(Number::from_str("1").unwrap()),
+                        ParserAst::new_constant(Number::from_str("5").unwrap()),
+                    ),
+                    ParserAst::new_pow(
+                        ParserAst::new_constant(Number::from_str("2").unwrap()),
+                        ParserAst::new_constant(Number::from_str("3").unwrap()),
+                    )
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_short() {
+        let input = "3+ 5 -  2";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_sub(
+                ParserAst::new_add_pair(
+                    ParserAst::new_constant(Number::from_str("3").unwrap()),
+                    ParserAst::new_constant(Number::from_str("5").unwrap()),
+                ),
+                ParserAst::new_constant(Number::from_str("2").unwrap())
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_float() {
+        let input = "3.14 + 2.71";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_add_pair(
+                ParserAst::new_constant(Number::from_str("3.14").unwrap()),
+                ParserAst::new_constant(Number::from_str("2.71").unwrap()),
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_invalid_input() {
+        let input = "3 + * 5";
+        let ast = parse(input);
+
+        assert!(ast.is_err(), "Expected parse to fail with invalid input");
+    }
+
+    #[test]
+    fn test_parse_expression_with_products() {
+        let input = "3 + 5 * 2";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_add_pair(
+                ParserAst::new_constant(Number::from_str("3").unwrap()),
+                ParserAst::new_mul_pair(
+                    ParserAst::new_constant(Number::from_str("5").unwrap()),
+                    ParserAst::new_constant(Number::from_str("2").unwrap())
+                ),
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_powers() {
+        let input = "7 * 2^3 + 4";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_add_pair(
+                ParserAst::new_mul_pair(
+                    ParserAst::new_constant(Number::from_str("7").unwrap()),
+                    ParserAst::new_pow(
+                        ParserAst::new_constant(Number::from_str("2").unwrap()),
+                        ParserAst::new_constant(Number::from_str("3").unwrap())
+                    )
+                ),
+                ParserAst::new_constant(Number::from_str("4").unwrap()),
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_parentheses() {
+        let input = "(3 + 5) * 2";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_mul_pair(
+                ParserAst::new_add_pair(
+                    ParserAst::new_constant(Number::from_str("3").unwrap()),
+                    ParserAst::new_constant(Number::from_str("5").unwrap()),
+                ),
+                ParserAst::new_constant(Number::from_str("2").unwrap())
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_nested_parentheses() {
+        let input = "3 + (2 * (5 - 1))^2";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_add_pair(
+                ParserAst::new_constant(Number::from_str("3").unwrap()),
+                ParserAst::new_pow(
+                    ParserAst::new_mul_pair(
+                        ParserAst::new_constant(Number::from_str("2").unwrap()),
+                        ParserAst::new_sub(
+                            ParserAst::new_constant(Number::from_str("5").unwrap()),
+                            ParserAst::new_constant(Number::from_str("1").unwrap())
+                        )
+                    ),
+                    ParserAst::new_constant(Number::from_str("2").unwrap())
+                ),
+            )
+        );
+    }
+
+    #[test]
+    fn test_parse_expression_with_function_call() {
+        let input = "5*pi^2/4*cos[pi*x/2]*sin[pi*y/2]";
+        let ast = parse(input).expect("Failed to parse expression");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_mul_pair(
+                ParserAst::new_mul_pair(
+                    ParserAst::new_div(
+                        ParserAst::new_mul_pair(
+                            ParserAst::new_constant(Number::from_str("5").unwrap()),
+                            ParserAst::new_pow(
+                                ParserAst::new_named_value("pi".to_string()),
+                                ParserAst::new_constant(Number::from_str("2").unwrap())
+                            )
+                        ),
+                        ParserAst::new_constant(Number::from_str("4").unwrap())
+                    ),
+                    ParserAst::new_cos(ParserAst::new_div(
+                        ParserAst::new_mul_pair(
+                            ParserAst::new_named_value("pi".to_string()),
+                            ParserAst::new_named_value("x".to_string())
+                        ),
+                        ParserAst::new_constant(Number::from_str("2").unwrap())
+                    ))
+                ),
+                ParserAst::new_sin(ParserAst::new_div(
+                    ParserAst::new_mul_pair(
+                        ParserAst::new_named_value("pi".to_string()),
+                        ParserAst::new_named_value("y".to_string()),
+                    ),
+                    ParserAst::new_constant(Number::from_str("2").unwrap())
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test_parsing_block_with_nested_expressions() {
+        let input = "{ 3 + 4; 5 * 6 }";
+        let ast = parse(input).expect("Failed to parse block with nested expressions");
+
+        assert_eq!(
+            ast,
+            ParserAst::new_block(vec![
+                ParserAst::new_add_pair(
+                    ParserAst::new_constant(Number::from_str("3").unwrap()),
+                    ParserAst::new_constant(Number::from_str("4").unwrap()),
+                ),
+                ParserAst::new_mul_pair(
+                    ParserAst::new_constant(Number::from_str("5").unwrap()),
+                    ParserAst::new_constant(Number::from_str("6").unwrap())
+                )
+            ])
+        );
+    }
+}
